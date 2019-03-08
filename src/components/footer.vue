b34ec69408a82f2e27647c5214df5c55562529b2
xhzdcyy <912575054@qq.com>
2019-03-08 15:33:33 +0800

    编写各个页面组件

---
+++ src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <p>@ xhzdcyy.com 2019</p>
+  </footer>
+</template>
+<style scoped>
+  footer {
+    align-self: end;
+    background-color: #d7d7d7;
+    color: #666;
+    font-size: 13px;
+    padding: 10px;
+    text-align: center;
+  }
+
+</style>
